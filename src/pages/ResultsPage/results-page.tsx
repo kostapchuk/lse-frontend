d22f02fd5266f5b47c9004933c97174303026b94
A S U S <borodichyulia18@gmail.com>
2022-09-07 19:06:21 +0300

    feat: add table

--- src/pages/ResultsPage/results-page.tsx
+++ src/pages/ResultsPage/results-page.tsx
@@ -1,38 +1,55 @@
-import React, {FC, useEffect, useState} from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import BasicTable from '../../components/results-table/results-table';
 
-interface IResults {
-    id: string;
-    quizName: string;
-    firstName: string;
-    lastName: string;
-    group: string;
-    faculty: string;
-    email: string;
-    score: number;
-    maxScore: number;
+export interface IResults {
+  id: string;
+  quizName: string;
+  firstName: string;
+  lastName: string;
+  group: string;
+  faculty: string;
+  email: string;
+  score: number;
+  maxScore: number;
 }
 
 const ResultsPage: FC = () => {
+  const [results, setResults] = useState<IResults[]>([]);
 
-    const [results, setResults] = useState<IResults[]>([]);
+  const resultUser = {
+    id: 1,
+    quizName: 'Test1',
+    firstName: 'Kirill',
+    lastName: 'Borodich',
+    group: '19',
+    faculty: 'fsk',
+    email: 'kurusha.myau@mail.ru',
+    score: '30',
+    maxScore: '500',
+  };
 
-    useEffect(() => {
-        subscribe()
-    }, [])
+  useEffect(() => {
+    subscribe();
+  }, []);
 
-    const subscribe = async () => {
-        const eventSource = new EventSource(`http://localhost:8080/api/v1/results/stream`)
-        eventSource.onmessage = function (event) {
-            const message = JSON.parse(event.data);
-            setResults(prev => [message, ...prev]);
-        }
-    }
+  const subscribe = async () => {
+    const eventSource = new EventSource(
+      `http://localhost:8080/api/v1/results/stream`
+    );
+    eventSource.onmessage = function (event) {
+      const message = JSON.parse(event.data);
+      setResults((prev) => [message, ...prev]);
+    };
+  };
 
-    return(
-        <>
-            {results.map(res => <p key={res.id}>{res.quizName + ":" + res.score}</p>)}
-        </>
-    )
+  return (
+    <BasicTable />
+    // <>
+    //   {results.map((res) => (
+    //     <p key={res.id}>{res.quizName + ':' + res.score}</p>
+    //   ))}
+    // </>
+  );
 };
 
 export default ResultsPage;
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,21 +1,33 @@
 export interface IQuestion {
-    id: string;
-    text: string;
-    multipleChoice: boolean;
+  id: string;
+  text: string;
+  multipleChoice: boolean;
 }
 
 export interface IQuizItem {
-    question: IQuestion;
-    answers: IAnswer[];
+  question: IQuestion;
+  answers: IAnswer[];
 }
 
 export interface IQuiz {
-    quizId: string;
-    quizName: string;
-    quizItems: IQuizItem[];
+  quizId: string;
+  quizName: string;
+  quizItems: IQuizItem[];
 }
 
 export interface IAnswer {
-    id: string;
-    text: string;
+  id: string;
+  text: string;
+}
+
+export interface IResults {
+  id: string;
+  quizName: string;
+  firstName: string;
+  lastName: string;
+  group: string;
+  faculty: string;
+  email: string;
+  score: number;
+  maxScore: number;
 }
